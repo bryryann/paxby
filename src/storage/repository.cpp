b490eb6afb8ba723ccaecf912f0187c548f28c42
bryryann <186111776+bryryann@users.noreply.github.com>
2026-02-07 08:35:41 -0300

    define get_all() function in JsonTaskRepository

--- src/storage/repository.cpp
+++ src/storage/repository.cpp
@@ -1,9 +1,45 @@
-#include <storage/json_task_repository.h>
+#include <fstream>
+#include <nlohmann/json.hpp>
+
+#include "storage/json_task_repository.h"
+#include "core/tasks.h"
+#include "core/tasks_json.h"
 
 namespace storage {
 
 JsonTaskRepository::JsonTaskRepository(const std::filesystem::path& app_dir)
     : file_path_(app_dir / "tasks.json") {}
+
+std::vector<core::Task> JsonTaskRepository::get_all() {
+    validate_storage();
+
+    std::ifstream in(file_path_);
+    nlohmann::json j;
+
+    try {
+        in >> j;
+    } catch (const nlohmann::json::parse_error& e) {
+        throw std::runtime_error(
+            "Failed to parse tasks.json (file may be corrupted)."
+        );
+    }
+
+    if (!j.contains("tasks") || !j["tasks"].is_array()) {
+        throw std::runtime_error("Invalid tasks file format");
+    }
+
+    return j.at("tasks").get<std::vector<core::Task>>();
+}
+
+void JsonTaskRepository::save_all(const std::vector<core::Task>& tasks) {
+    validate_storage();
+
+}
+
+void JsonTaskRepository::add(const core::Task& task) {
+    validate_storage();
+
+}
 
 void JsonTaskRepository::validate_storage() {
     if (!std::filesystem::exists(file_path_)) {
@@ -11,4 +47,4 @@ void JsonTaskRepository::validate_storage() {
     }
 }
 
-}
+} // namespace storage
